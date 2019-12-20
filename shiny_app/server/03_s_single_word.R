796d014610d0ecd97c51a2334e339c9c4dff097e
RichardHHill <richard_hill@brown.edu>
2019-12-19 19:50:41 -0500

    Word Breakdown

--- shiny_app/server/03_s_single_word.R
+++ shiny_app/server/03_s_single_word.R
@@ -21,17 +21,58 @@ observeEvent(input$submit_single_word, {
   word <- input$check_single_word
   
   genre <- complete_lexique %>% 
-    filter(word == word) %>% 
+    filter(word == !!word) %>% 
     pull(genre)
+  
+  table_prep <- bind_rows(lapply(seq_len(min(5, nchar(word))), function(x) {
+    lexique_data[[x]] %>% 
+      filter(suffixe == substr(word, nchar(word) - x + 1, nchar(word))) %>% 
+      mutate(percent_male = male / (male + femelle) * 100) %>% 
+      select(suffixe, male, femelle, percent_male)
+  }))
+  
+  if (length(genre) == 0) {
+    genre <- ""
+    word <- "Word not found"
+  }
+  
+  
+  output$word_breakdown_table <- renderDT({
+    datatable(
+      table_prep,
+      rownames = FALSE,
+      colnames = c("Suffixe", "Male", "Femelle", "% Male")
+    )
+  })
+  
+  output$word_breakdown_chart <- renderHighchart({
+    
+    highchart() %>% 
+      hc_chart(type = "line") %>% 
+      hc_yAxis(min = 0, max = 100) %>% 
+      hc_add_series(
+        table_prep$percent_male
+      )
+  })
   
   output$word_breakdown <- renderUI({
     fluidRow(
-      column(
-        12,
-        align = "center",
-        h2(word),
-        br(),
-        h3(paste0("Genre: ", genre))
+      box(
+        column(
+          12,
+          align = "center",
+          h2(word),
+          br(),
+          h3(paste0("Genre: ", genre)),
+          br(),
+          DTOutput("word_breakdown_table")
+        )
+      ),
+      box(
+        column(
+          12,
+          highchartOutput("word_breakdown_chart")
+        )
       )
     )
   })
